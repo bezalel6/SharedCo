6ef7f8d3a36f739ab86461fd055137e106eed71f
bezalel6 <bezalel3250@gmail.com>
2021-03-14 11:42:58 +0200

    SSS

--- Assembly/BIN/2014_8.asm
+++ Assembly/BIN/2014_8.asm
@@ -1,17 +1,48 @@
-cr equ 13
 .MODEL small
 .STACK 100h
 .DATA
-    A db 7,3,4,5,6,2
+    A db 5,8,9
     LEN=$-A
     P DW A,LEN
-    B DW LEN
 .CODE
-start:
+start:s
     mov ax, @data
     mov ds, ax
-
-exit:
-    mov ah, 4ch
+    push P
+    push P+2
+    call trouble
+    mov ah, 2
+    mov di, offset A
+    mov cx, LEN
+print:
+    mov dl, [di]
+    add dl,'0'
     int 21h
+    inc di
+    loop print
+sof:
+    mov ah,4ch
+    int 21h
+trouble:
+    push bp
+    mov bp,sp
+chazor:
+    mov cx,[bp+4]
+    dec cx
+    xor si,si
+    mov bx,[bp+6]
+again:
+    mov al,[bx]
+    cmp al,[bx+1]
+    jbe con
+    xchg al,[bx+1]
+    mov [bx],al
+    inc si
+con:
+    inc bx
+    loop again
+    or si,si
+    jne chazor
+    pop bp
+    ret 4
 END start
